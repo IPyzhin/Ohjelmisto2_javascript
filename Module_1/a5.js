0744e44a7ce7b5acb4bec3f53877ece26e786661
IPyzhin <ivan.pyzhin@gmail.com>
2026-03-25 09:20:48 +0200

    JS a5 tehtävä

---
+++ Module_1/a5.js
@@ -0,0 +1,7 @@
+let year = parseInt(prompt("Enter a year: "));
+let result = document.querySelector('#result');
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    result.innerHTML = year + " is a leap year";
+} else {
+    result.innerHTML = year + " is not a leap year";
+}
