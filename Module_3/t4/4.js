7405d63a5ff8dc14d1342b0ae4c6807767d3eccd
GentleDelirium <ivan.pyzhin@gmail.com>
2026-04-03 14:53:00 +0300

    js code added to t4

--- Module_3/t4/4.js
+++ Module_3/t4/4.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 const students = [
   {
     name: 'John',
@@ -12,4 +12,12 @@ const students = [
     name: 'Jones',
     id: '5423679',
   },
-];
+]
+const target = document.querySelector('#target')
+for (let i = 0;i < students.length; i++) {
+  let option = document.createElement('option')
+  option.value = students[i].id
+  let name = document.createTextNode(students[i].name)
+  option.appendChild(name)
+  target.appendChild(option)
+  }
